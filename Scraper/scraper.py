32f3c446ffea5f6cee76dda271fb92459ac94610
Tailfery <tailfery@gmail.com>
2022-12-13 09:05:19 -0800

    Trying filtering of post

--- Scraper/scraper.py
+++ Scraper/scraper.py
@@ -1,6 +1,8 @@
 import instaloader
+import re
 
 L = instaloader.Instaloader()
+
 
 #kait.drawsthings
 # Replace this with login we need
@@ -11,9 +13,12 @@ L = instaloader.Instaloader()
 # Currently Hardcoded to Kaits profile - Change to list eventually
 profile = instaloader.Profile.from_username(L.context, "kait.drawsthings")
 
-# Get whatever info from posts in a recent timeframe
-# Maybe just go since the last call? 24hours?
-#profile.get_posts()
+# Download the profile and filter using regular expressions for the phrases books open, opening books, etc. 
+# Can clean this up later for a better regular expression but try this for now
+
+L.download_profiles(profile.get_posts(), 
+post_filter=lambda post: re.search("(books)+.*(open)+|(open)+.*(books)+|(opening)+.*(books)+|(books)+.*(opening)+", post.caption.lower()),
+max_count=1)
 
 for post in profile.get_posts():
     L.download_post(post, "kait.drawsthings")
