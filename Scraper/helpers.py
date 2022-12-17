27a054ad0352bc148453e6beeed3c0106e9dd202
Tailfery <tailfery@gmail.com>
2022-12-17 13:45:13 -0800

    Discord functionality added

--- Scraper/helpers.py
+++ Scraper/helpers.py
@@ -1,6 +1,10 @@
 import re
+from discordwebhook import Discord
 
+# Regex pattern for searching the posts
 pattern = "(books)+.*(open)+|(open)+.*(books)+|(opening)+.*(books)+|(books)+.*(opening)+"
+# Discord object using the webhook given for a server
+discord = Discord(url="https://discord.com/api/webhooks/1053724148054114376/qpZY98-hAEWl0qfxw1Lav2heLZP7t5xyL9WhltfsYBoJuMiDG2YuMKNSY1IYZSdz_oBw")
 
 # Short little helper to clean up code in the download_profiles step
 def post_filter_helper(post):
@@ -9,3 +13,18 @@ def post_filter_helper(post):
         match_object = (re.search(pattern, post.caption.lower()))
         if match_object != None:
             return match_object.group()
+
+
+# For posting to Discord for notifying when books are open
+def discord_notify(caption, name):
+    discord.post(embeds=[
+        {
+            "author": {
+                "name": name
+            },
+            "title": "Books are open!",
+            "description": caption,
+        }
+    ]
+    )
+    return True
--- Scraper/scraper.py
+++ Scraper/scraper.py
@@ -1,39 +1,31 @@
 import instaloader
 import helpers
+import glob
+import os
 
+# Create an instaloader object to work wtih
 L = instaloader.Instaloader()
 
-
-#kait.drawsthings
-# Replace this with login we need
-#L.login(USER, PASSWORD) 
-#L.interative_login(USER)
-#L.load_session_from_file(USER)
 
 # List of usernames to download
 string_list = ["wraith________", "moss.meadows"]
 profile_list = []
 
-
-# Currently Hardcoded to Kaits profile - Change to list eventually
+# Iterate through the list of usernames and create profiles to download later
 for profile in string_list:
     profile_list.append(instaloader.Profile.from_username(L.context, profile))
 
 
 # Download the profile and filter using regular expressions for the phrases books open, opening books, etc. 
-# Can clean this up later for a better regular expression but try this for now
 # Filter out closed for instances where it mentions both closed books but opening at a later date
 
-L.download_profiles(profiles=profile_list,
+L.download_profiles(profiles=profile_list, fast_update=True,
 post_filter=lambda post: not "closed" in helpers.post_filter_helper(post))
 
-
-# for post in profile.get_posts():
-#    L.download_post(post, "kait.drawsthings")
-
-# Store data
-#profile.storeData()
-
-# Filter Data
-#profile.FilterData()
-
+# Temporarily I have it grabbing the most recent .txt file but eventually should just grab
+# the file if its new. It then calls up the discordwebhook module to send out the notification
+for username in string_list:
+    file_list = glob.glob('I:/Documents/Coding_Projects/Insta_scraper/Scraper/' + username +'/*.txt')
+    recent_file = min(file_list, key=os.path.getctime)
+    caption = open(recent_file, 'r', encoding="utf8").read()
+    helpers.discord_notify(caption, username)
